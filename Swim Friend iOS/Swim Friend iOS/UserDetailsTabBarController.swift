de74debdda8f3ca1d9120d19976bdd13cf3323c3
EmperorLeo <laf22@duke.edu>
2015-12-17 15:11:21 -0500

    added blue tint to tab bar

---
+++ Swim Friend iOS/Swim Friend iOS/UserDetailsTabBarController.swift
@@ -0,0 +1,23 @@
+//
+//  UserDetailsTabBarController.swift
+//  Swim Friend iOS
+//
+//  Created by Leo Feldman on 12/17/15.
+//  Copyright © 2015 Leo and Ray. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UserDetailsTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        self.tabBar.backgroundColor = BLUE_THEME
+    }
+    
+    override func didReceiveMemoryWarning() {
+
+    }
+    
+    
+}
