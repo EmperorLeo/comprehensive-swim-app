6c1838d605125b4b7c0adf404f2aed5e2b7c75bf
EmperorLeo <laf22@duke.edu>
2015-12-17 03:57:33 -0500

    uploaded ios and osx apps

---
+++ Swim Friend OSX/Swim Friend OSX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Swim Friend OSX
+//
+//  Created by Leo Feldman on 12/17/15.
+//  Copyright © 2015 Leo and Ray. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
